3eccb12d9f2176fe8cabad6b200637c9d4e866fe
Sriram MK <mksriram@umich.edu>
2025-04-05 17:42:06 -0400

    removed files for go

---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+// pacakage main must have the main function here 
+
+//go run test.go , go build -> ./file.go
+
+func main() { 
+
+
+
+	const mainVariable = "preimumPlan"
+	const differentVariable ="preimumPlan" // given the value i would change it 
+}
--- todo/src/hooks/useGetwords.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export const useGetWords = (word) => {
-  const [words, setWords] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  return { isLoading, synonyms, getSynonyms };
-};
