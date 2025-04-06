abce7bbb709b557155954c7da684c48dfc349f67
Sriram MK <mksriram@umich.edu>
2025-04-06 17:06:40 -0400

    added go

--- test.go
+++ test.go
@@ -1,5 +1,6 @@
 package main
-// pacakage main must have the main function here 
+
+// pacakage main must have the main function here
 
 //go run test.go , go build -> ./file.go
 
@@ -9,4 +10,64 @@ func main() {
 
 	const mainVariable = "preimumPlan"
 	const differentVariable ="preimumPlan" // given the value i would change it 
+
 }
+
+func add(value1 int, value2 int ) int { 
+
+	return value1 + value2
+}
+type Celsius float64 
+type Fahrenheit float64 
+
+
+/*
+func nameOfFunction(Value inputDataType) outputDataType {
+
+}
+
+
+type User struct {
+	Name string 
+}
+
+//methods ( these attached to user ) ( like User.sayHello())
+func (u *User) sayHello() { 
+
+}
+
+func sayHello(u *User) { 
+
+}
+
+
+
+func ( u *User) sayHello(){ 
+fmt.printf("hellow workd %d", value)
+
+	}
+
+	this 
+
+
+
+	interfaces 
+type shape interace { 
+
+	area() float64 
+	perimeter() float64 
+	width, height flaot64
+}
+*/
+
+type Wheel struct { 
+
+	name string 
+	mileage float64
+	model string 
+	year int 
+
+}
+func temperatureConversion(c Celsius) Fahrenheit {
+
+} 
